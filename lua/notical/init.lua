e4471fa90d8c425e394f28d29e4c9a78a49c8655
Brennon Loveless <brennon.loveless@gmail.com>
2022-08-01 08:37:47 -0700

    Initial commit

--- lua/notical/init.lua
+++ lua/notical/init.lua
@@ -1,1 +1,15 @@
-print("Hello from notical")
+M = {}
+
+M.setup = function()
+    vim.api.nvim_create_user_command('Notical', function(opts)
+        M.hello()
+    end, {})
+end
+
+function M.hello()
+    print("hello from notical without save")
+end
+
+M.hello()
+
+return M
