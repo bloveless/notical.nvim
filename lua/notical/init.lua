8b04b9ca98004f3594015d9206d962f47865d074
Brennon Loveless <brennon.loveless@gmail.com>
2022-08-02 21:18:13 -0700

    Added some code for a popup window

--- lua/notical/init.lua
+++ lua/notical/init.lua
@@ -1,15 +1,37 @@
 M = {}
 
 M.setup = function()
-    vim.api.nvim_create_user_command('Notical', function(opts)
-        M.hello()
-    end, {})
+    print("Notical has been setup")
 end
 
-function M.hello()
-    print("hello from notical without save")
+M.hello = function()
+    print("hello from notical another test, yet another test")
+end
+
+M.popup = function()
+    local width = 50
+    local height = 10
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    local current_ui = vim.api.nvim_list_uis()[1]
+
+    P(current_ui)
+
+    local opts = {
+        relative = 'editor',
+        width = width,
+        height = height,
+        col = (current_ui.width / 2) - (width / 2),
+        row = (current_ui.height / 2) - (height / 2),
+        anchor = 'NW',
+        style = 'minimal',
+        focusable = false,
+    }
+
+    vim.api.nvim_open_win(buf, 1, opts)
 end
 
 M.hello()
+M.popup()
 
 return M
